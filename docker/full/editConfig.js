fa0dd8fbab9090bdc4cd274badcc0f5c22f2f6eb
Ian Mackie <ianm@completesystemsupport.com>
2019-05-30 09:30:20 -0400

    updated base configs and docker install

--- docker/full/editConfig.js
+++ docker/full/editConfig.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const config = JSON.parse(fs.readFileSync('jsreport.config.json').toString())
+config.extensions.scripts.allowedModules = ['handlebars-helpers', 'https', 'lodash', 'moment']
 delete config.extensions['sample-template']
 delete config.extensions['authentication']
 fs.writeFileSync('jsreport.config.json', JSON.stringify(config))
